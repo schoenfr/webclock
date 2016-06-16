b4cfc65e6778ef294637951f4582dfce75155cad
René Schönfelder <Ren Schnfelder>
2016-06-17 00:32:25 +0200

    docker 3

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Hey, hello world!");
     System.out.println("One more step");
+    System.out.println("for mankind!");
   }
 
 }
